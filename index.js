fec5de988e74702703313e0b37356aa5b2d33978
Joao-Pedro.PIRES-QUEIROZ <Joao-Pedro.PIRES-QUEIROZ@akka.eu>
2021-09-05 15:23:35 +0200

    Connect to POSTGRESQL table, insert row from code

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const PORT = 8150;
 
 const got = require('got');
 
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'joaodb',
+  password: 'Platipus1_',
+  port: 5432,
+})
+
 // Map user friendly request names, into numbers
 var map_characteristic = {
     height: 1,
@@ -11,6 +20,17 @@ var map_characteristic = {
     direction: 3,
     temperature: 4
 };
+
+const createUser = (request, response) => {  
+    pool.query('INSERT INTO buoys (date, max_height, significant_height, avg_period, peak_period, direction, temperature)\
+                VALUES ($1, $2, $3, $4, $5, $6, $7)', ['2020-10-05 14:01:10', 1.1, 0.9, 7.3, 8, 358, 24.5], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${results.insertId}`)
+    })
+}
+createUser
 
 async function get_buoys_data(path, transformation_type){
     let resp = {}
