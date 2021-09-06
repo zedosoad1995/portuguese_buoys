8dfb3629f0a6b357f76dba00a3e5286ec09bae48
Joao-Pedro.PIRES-QUEIROZ <Joao-Pedro.PIRES-QUEIROZ@akka.eu>
2021-09-07 00:59:07 +0200

    Concatenates new data with the main data

--- queries.js
+++ queries.js
@@ -21,7 +21,54 @@ function check_null(val){
     return val
 }
 
-// TODO: Insert everything with just 1 INSERT
+const update_query = "UPDATE buoys \
+SET max_height = t3.max_height, \
+significant_height = t3.significant_height, \
+avg_period = t3.avg_period, \
+peak_period = t3.peak_period, \
+direction = t3.direction, \
+temperature = t3.temperature \
+FROM( \
+SELECT \
+t1.date AS date, \
+CASE \
+WHEN (t2.max_height IS NULL) THEN t1.max_height \
+ELSE t2.max_height \
+END AS max_height, \
+CASE \
+WHEN (t2.significant_height IS NULL) THEN t1.significant_height \
+ELSE t2.significant_height \
+END AS significant_height, \
+CASE \
+WHEN (t2.avg_period IS NULL) THEN t1.avg_period \
+ELSE t2.avg_period \
+END AS avg_period, \
+CASE \
+WHEN (t2.peak_period IS NULL) THEN t1.peak_period \
+ELSE t2.peak_period \
+END AS peak_period, \
+CASE \
+WHEN (t2.direction IS NULL) THEN t1.direction \
+ELSE t2.direction \
+END AS direction, \
+CASE \
+WHEN (t2.temperature IS NULL) THEN t1.temperature \
+ELSE t2.temperature \
+END AS temperature \
+FROM temp_buoys t1 \
+INNER JOIN buoys t2 ON t2.date = t1.date \
+WHERE t2.max_height IS NULL OR t2.significant_height IS NULL OR \
+t2.avg_period IS NULL OR t2.peak_period IS NULL OR \
+t2.direction IS NULL OR t2.temperature IS NULL \
+) AS t3 \
+WHERE t3.date = buoys.date"
+
+const insert_query = "INSERT INTO buoys (date, max_height, significant_height, avg_period, peak_period, direction, temperature) \
+SELECT t1.date, t1.max_height, t1.significant_height, t1.avg_period, t1.peak_period, t1.direction, t1.temperature \
+FROM temp_buoys t1 \
+LEFT JOIN buoys t2 ON t2.date = t1.date \
+WHERE t2.id IS NULL"
+
 var Param_names = ["DATE", "HMAX", "HS", "TZ", "TP", "THTP", "TEMP"]
 async function insertBuyosData(request, response){
     const obj = request.body
@@ -42,8 +89,19 @@ async function insertBuyosData(request, response){
             if (error) {
                 throw error
             }
+            pool.query(update_query, (error, results) => {
+                if (error) {
+                    throw error
+                }
+                pool.query(insert_query, (error, results) => {
+                    if (error) {
+                        throw error
+                    }
+                })
+            })
         })
     })
+
 
     response.status(201).send("Success")
 }
